f52448d48a12063c679bcf27e607192d335f9976
Jonathan Rodriguez <rjonathan@outlook.es>
2025-02-10 02:11:35 -0400

    chore(style): Configuration structural project

--- OrderApp/src/router.tsx
+++ OrderApp/src/router.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Products from './views/Products'
+import NewProduct from './views/NewProduct'
 
-const router = () => {
-  return (
-    <div>router</div>
-  )
-}
 
-export default router
+export const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />, 
+      children: [
+        {
+          index: true,
+          element: <Products />,
+      },{
+        path: 'productos/nuevo',
+        element: <NewProduct />,
+    },
+      ]
+    }
+  ])
